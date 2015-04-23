af1536e228c7770e44f1f1a74cb53508e67698bd
Mirai Akagawa <mirai418@me.com>
2015-04-23 13:33:05 -0400

    Firing local notification with retrievable ID

--- leaflet/AppDelegate.swift
+++ leaflet/AppDelegate.swift
@@ -8,42 +8,63 @@
 
 import UIKit
 
+/**
+* AppDelegate, which also acts as the CLLocationManagerDelegate, meaning any system notifications to do with locations will get handled in this class.
+* To avoid clogging, created an extension which handles the CLLocationManagerDelegate specifically, see below.
+*/
 @UIApplicationMain
 class AppDelegate: UIResponder, UIApplicationDelegate, CLLocationManagerDelegate {
 
     // CONSTANTS
     let googleMapsApiKey = "AIzaSyAoKcrRBul4PmuGDzUfPKAi1_KoA0StmJQ"
+    
+    // TODO: generate a new uuid just for this application to use and set all beacons with this uuid
     // Default uuid string for estimote beacons
     let uuidString = "B9407F30-F5F8-466E-AFF9-25556B57FE6D"
     let beaconIdentifier = "parks-and-rec.cmu.edu.Leaflet"
     
-    var window: UIWindow?
+    // location manager will handle region monitoring and changes in beacon region
     var locationManager: CLLocationManager?
 
+    var window: UIWindow?
+
+    // an array of known beacons. // TODO: move this to API
     var knownBeacons = [NSNumber: CLProximity]()
     
 
     func application(application: UIApplication, didFinishLaunchingWithOptions launchOptions: [NSObject: AnyObject]?) -> Bool {
-        // Override point for customization after application launch.
+
+        // TODO: remove google maps dependency, including all framework files
         GMSServices.provideAPIKey(googleMapsApiKey)
         
+        /************** BEACON MONITORING SETUP **************/
+        
+        // set up location manager which will handle beacon regions, etc.
+        locationManager = CLLocationManager()
+        locationManager!.delegate = self
+        locationManager!.pausesLocationUpdatesAutomatically = false
+        
+        // ask user permission to use location stuff
+        if locationManager!.respondsToSelector("requestAlwaysAuthorization") {
+            locationManager!.requestAlwaysAuthorization()
+        }
+        
+        // set beacon region based on the uuid string constant
+        // note that by fixing the uuid and controlling major/minor values, we only need to monitor one region
         let beaconUUID:NSUUID = NSUUID(UUIDString: uuidString)!
         let beaconRegion:CLBeaconRegion = CLBeaconRegion(proximityUUID: beaconUUID,
             identifier: beaconIdentifier)
         
-        locationManager = CLLocationManager()
-        
-        if(locationManager!.respondsToSelector("requestAlwaysAuthorization")) {
-            locationManager!.requestAlwaysAuthorization()
-        }
-        
-        locationManager!.delegate = self
-        locationManager!.pausesLocationUpdatesAutomatically = false
-        
+        // start monitoring and updating for the specified region.
         locationManager!.startMonitoringForRegion(beaconRegion)
         locationManager!.startRangingBeaconsInRegion(beaconRegion)
         locationManager!.startUpdatingLocation()
         
+        
+        /************** REGISTER NOTIFICATION **************/
+        
+        // Register local notifications. Currently this app does not use remote notifications.
+        // Also ask permission if not done already.
         if(application.respondsToSelector("registerUserNotificationSettings:")) {
             application.registerUserNotificationSettings(
                 UIUserNotificationSettings(
@@ -53,18 +74,32 @@ class AppDelegate: UIResponder, UIApplicationDelegate, CLLocationManagerDelegate
             )
         }
         
-        if let options = launchOptions {
-            if let notification = options[UIApplicationLaunchOptionsLocalNotificationKey] as? [NSObject : AnyObject] {
-                println("So now it works.")
-                //notification found mean that you app is opened from notification
-            }
-        }
-        
+
+
         return true
     }
-
+    
+    /**
+    * This is the app delegate method that catches how the app responds when a local notification is fired.
+    * Here, one of two things can happen:
+    *   1. The app was opened from a notification, and has emerged to the foreground process
+    *   2. The app was already running in the foreground, and a notification was fired. (Does not actually show up as a notification)
+    */
     func application(application: UIApplication, didReceiveLocalNotification notification: UILocalNotification) {
-        NSLog("FIREEEEEEE")
+        NSLog("---> didReceiveLocalNotification")
+        switch application.applicationState {
+            case UIApplicationState.Active:
+                NSLog("Active")
+            case UIApplicationState.Background:
+                NSLog("Background")
+            case UIApplicationState.Inactive:
+                NSLog("Inactive")
+        }
+        if notification.userInfo != nil && notification.userInfo!["poiId"] != nil {
+            let poiId:Int = notification.userInfo!["poiId"] as! Int
+            NSLog("POI Id is: \(poiId)")
+        }
+
     }
     
     func applicationWillResignActive(application: UIApplication) {
@@ -94,17 +129,26 @@ class AppDelegate: UIResponder, UIApplicationDelegate, CLLocationManagerDelegate
 
 extension AppDelegate: CLLocationManagerDelegate {
     
-
-    // actual method that registers the local notification.
-    func sendLocalNotificationWithMessage(message: String!, playSound: Bool) {
+    /*
+    * actual method that registers the local notification.
+    */
+    func sendLocalNotificationWithMessage(message: String!, playSound: Bool, userInfo: [String:Int?]) {
         
+        // construct the notification with a message.
         let notification:UILocalNotification = UILocalNotification()
         notification.alertBody = message
         
+        // we can attach an id to the notification so we know which poi to retrieve when the user opens.
+        if let id = userInfo["poiId"] {
+            notification.userInfo = ["poiId": id!]
+        }
+
+        // play a custom sound when the notification fires
         if(playSound) {
             notification.soundName = "tos_beep.caf";
         }
         
+        // send
         UIApplication.sharedApplication().scheduleLocalNotification(notification)
     }
     
@@ -113,8 +157,9 @@ extension AppDelegate: CLLocationManagerDelegate {
         //            viewController.beacons = beacons as [CLBeacon]?
         //            viewController.tableView!.reloadData()
 
-        NSLog("didRangeBeacons: \(beacons.count)");
+
         var message:String = ""
+//                NSLog("didRangeBeacons: \(beacons.count)");
         var foundBeacon:CLBeacon
         
         for beacon in beacons {
@@ -128,7 +173,8 @@ extension AppDelegate: CLLocationManagerDelegate {
                     if (poi?.visit == false) {
                         message = "Your are near the " + poi!.title + "!"
                         //                    message = "You are near beacon with major \(foundBeacon.major) and minor \(foundBeacon.minor)"
-                        sendLocalNotificationWithMessage(message, playSound: true)
+                        var userInfo = ["poiId": poi?.id]
+                        sendLocalNotificationWithMessage(message, playSound: true, userInfo: userInfo)
                     }
                     poi?.setVisiting(true)
 
@@ -138,10 +184,10 @@ extension AppDelegate: CLLocationManagerDelegate {
 //            } else {
 //                knownBeacons[foundBeacon.major] = foundBeacon.proximity
 //            }
-            NSLog("beacon \(foundBeacon.major): proximity: \(foundBeacon.proximity.rawValue)")
+//            NSLog("beacon \(foundBeacon.major): proximity: \(foundBeacon.proximity.rawValue)")
         }
 
-        NSLog("known beacons: \(knownBeacons.count)")
+//        NSLog("known beacons: \(knownBeacons.count)")
     }
     
     func locationManager(manager: CLLocationManager!,
